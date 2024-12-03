5df19a1e2a85c8de898ecab8636d91c3219f0a32
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-12-03 12:33:06 +0530

    JavaScript Basics

---
+++ JavaScript/script.js
@@ -0,0 +1,65 @@
+// Javascript is dynamic programming language
+
+// varibales - var, let, const
+const email = "prasanth@thopstech.com";
+var firstName = "Prasanth";
+let lastName = "Kumar";
+
+/*
+Datatypes
+1. Number
+2. String
+3. Boolean
+4. Undefined
+*/
+let langName = "Java";
+// console.log(`I love ${langName}`);
+
+// console.log(2 + +"2a");
+
+// Loops - for, while, do while
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// Arrays
+let arr = [1, true, "two", [1, 2, 3]];
+// let b = [...arr];
+// b[1] = false;
+// arr[10] = "ten";
+// let ele = arr.push("55");
+// let ele = arr.pop();
+// console.log(arr);
+
+// for (let element of arr) {
+//     console.log(element);
+// }
+
+// Objects
+// let student = {
+//     name: "Chareesh",
+//     age: 22,
+//     gender: "male",
+// };
+
+// console.log(student.gender);
+
+let employees = [
+    {
+        empId: 1,
+        name: "Ram",
+        subject: ["Java", "Spring"],
+    },
+    {
+        empId: 2,
+        name: "Madhan",
+        subject: ["SQL", "Java", "Spring"],
+    },
+];
+console.log(employees[0].subject[0]);
